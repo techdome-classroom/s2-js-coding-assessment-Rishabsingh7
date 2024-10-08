c167032c460d797a466e08064e902a96e74b3c17
Rishabsingh7 <rishabsingh78089@gmail.com>
2024-10-08 14:35:39 +0530

    Done both program

--- program1.js
+++ program1.js
@@ -3,8 +3,25 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
-};
+        const stack = [];
+        const matchingBracket = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        };
+        
+        for (let char of s) {
+            if (char === '(' || char === '{' || char === '[') {
+                stack.push(char);
+            } else {
+                if (stack.length === 0 || stack.pop() !== matchingBracket[char]) {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.length === 0;
+    };
 
 module.exports = { isValid };
 
--- program2.js
+++ program2.js
@@ -3,6 +3,27 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const romanMap = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+        'C': 100, 'D': 500, 'M': 1000
+    };
+    let total = 0; 
+    let length = s.length;
+
+   
+    for (let i = 0; i < length; i++) {
+       
+        if (i + 1 < length && romanMap[s[i]] < romanMap[s[i + 1]]) {
+            total -= romanMap[s[i]];
+        } else {
+            total += romanMap[s[i]];
+        }
+    }
+    
+    return total;
+
+
+
     
 };
 
